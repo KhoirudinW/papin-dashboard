c6207409be60b7129c9ccdeaec69cf0da672fa3d
[KhoirudinW] <[kwidiyansa@gmail.com]>
2026-01-14 14:31:02 +0700

    kembalikan path semula

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation';
 
 export default function RootPage() {
   // Langsung arahkan ke /login yang ada di dalam grup (auth)
-  redirect('/(auth)/login');
+  redirect('/login');
 }
